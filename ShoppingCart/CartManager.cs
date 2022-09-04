e7745c6a7866da9b90bed7fa9303173467694606
Bekir PİRALP <bpir33@gmail.com>
2022-09-04 18:02:29 +0300

    ilk testler yapıldı

---
+++ ShoppingCart/CartManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingCart
+{
+    /**
+     * Gereksinimler
+     * 1. Sepete ürün eklene bilmelidir
+     * 2. Seppette olan ürün çıkarılabilmelidir
+     * 3. Sepet temzilenebilmelidir
+     */
+    public class CartManager
+    {
+        private readonly List<CartItem> _sepet;
+
+        public CartManager()
+        {
+            _sepet = new List<CartItem>();
+        }
+
+        public void Ekle(CartItem Urun)
+        {
+            if (Urun != null && Urun.Product != null)
+            {
+                if (Urun.Quantity <= 0)
+                    Urun.Quantity = 1;
+
+                var urunListe = _sepet.FirstOrDefault(i => i.Product.Id == Urun.Product.Id);
+                if (urunListe != null)
+                {
+                    urunListe.Quantity += Urun.Quantity;
+                }
+                else
+                {
+                    _sepet.Add(Urun);
+                }
+            }
+
+        }
+
+        public void Cikar(CartItem Urun)
+        {
+            var urunSepet = _sepet.FirstOrDefault(i => i.Product.Id == Urun.Product.Id);
+            if (urunSepet != null)
+            {
+                if (urunSepet.Quantity > Urun.Quantity && Urun.Quantity != 0)
+                {
+                    urunSepet.Quantity -= Urun.Quantity;
+                }
+                else if (urunSepet.Quantity <= Urun.Quantity)
+                {
+                    _sepet.Remove(urunSepet);
+                }
+                else
+                {
+                    urunSepet.Quantity--;
+                    if (urunSepet.Quantity <= 0)
+                        _sepet.Remove(urunSepet);
+                }
+            }
+        }
+
+        public void temizle()
+        {
+            _sepet.Clear();
+        }
+
+        public List<CartItem> Sepet
+        {
+            get
+            {
+                return _sepet;
+            }
+        }
+
+        public int ToplamUrunCesidi
+        {
+            get
+            {
+                return _sepet.Count;
+            }
+        }
+
+        public decimal ToplamFiyat
+        {
+            get
+            {
+                return _sepet.Sum(i => i.Quantity * i.Product.UnitPrice);
+            }
+        }
+
+        public int ToplamUrun
+        {
+            get
+            {
+                return _sepet.Sum(i => i.Quantity);
+            }
+        }
+    }
+}
---
+++ ShoppingCartTests/CartManagerTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass()]
+    public class CartManagerTests
+    {
+        
+        [TestMethod()]
+        public void Sepete_urun_eklene_bilmelidir()
+        {
+            // Arrange 
+            const int beklenen = 1; // sepette ürün yok oyüzden ilk ekleme de beklenen
+
+            var cartManager = new CartManager();
+            var cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    Id = 1,
+                    Name = "Laptop",
+                    UnitPrice= 3000
+                },
+                Quantity = 1
+            };
+
+            //Act
+            cartManager.Ekle(cartItem);
+            var toplamElemanSayısı = cartManager.ToplamUrun;
+
+            //Assert
+            Assert.AreEqual(beklenen, toplamElemanSayısı);
+        }
+
+        [TestMethod()]
+        public void Seppette_olan_urun_cikarilabilmelidir()
+        {
+            // Arrange 
+            var cartManager = new CartManager();
+            var cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    Id = 1,
+                    Name = "Laptop",
+                    UnitPrice = 3000
+                },
+                Quantity = 1
+            };
+
+            var beklenenElemanSayisi = cartManager.ToplamUrun;
+
+            cartManager.Ekle(cartItem);
+
+            //Act
+            cartManager.Cikar(cartItem);
+            var toplamElemanSayısı = cartManager.ToplamUrun;
+
+            //Assert
+            Assert.AreEqual(beklenenElemanSayisi, toplamElemanSayısı);
+        }
+
+        [TestMethod()]
+        public void Sepet_temzilenebilmelidir()
+        {
+            // Arrange 
+            const int beklenenEkle = 1;
+            const int beklenen = 0;
+
+            var cartManager = new CartManager();
+            var cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    Id = 1,
+                    Name = "Laptop",
+                    UnitPrice = 3000
+                },
+                Quantity = 1
+            };
+
+            //Act
+            cartManager.Ekle(cartItem);
+            var toplamElemanSayısı1 = cartManager.ToplamUrun;
+            cartManager.temizle();
+            var toplamElemanSayısı = cartManager.ToplamUrun;
+            //Assert
+            Assert.AreEqual(beklenenEkle, toplamElemanSayısı1);
+            Assert.AreNotEqual(toplamElemanSayısı, toplamElemanSayısı1);
+            Assert.AreEqual(beklenen, toplamElemanSayısı);
+        }
+
+    }
+}
