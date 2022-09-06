07cd993ba7189786549db806bbe9f8b1977f2b29
Bekir PİRALP <bpir33@gmail.com>
2022-09-06 19:51:20 +0300

    Assembyl yani derleme methodları bir test projesinde sadece 1 ertane intialize/kurulum cleanup/temizleme methodları olabilir

---
+++ AssemblyLevel/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace AssemblyLevel
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext testContext)
+        {
+            Debug.WriteLine("Derleme başlangıç metodu çalıştı");
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            Debug.WriteLine("Derleme Temizleme metodu çalıştı");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Debug.WriteLine("birim testi 1 : Sınıf Temizleme metodu çalıştı");
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            Debug.WriteLine("birim testi 1 : Sınıf başlangıç metodu çalıştı");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Debug.WriteLine("birim testi 1 : test metodu 1 çalıştı");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Debug.WriteLine("birim testi 1 : test metodu 2 çalıştı");
+        }
+    }
+}
---
+++ AssemblyLevel/UnitTest2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace AssemblyLevel
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Debug.WriteLine("birim testi 2 : Sınıf Temizleme metodu çalıştı");
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            Debug.WriteLine("birim testi 2 : Sınıf başlangıç metodu çalıştı");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Debug.WriteLine("birim testi 2 : test metodu 1 çalıştı");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Debug.WriteLine("birim testi 2 : test metodu 2 çalıştı");
+        }
+    }
+}
