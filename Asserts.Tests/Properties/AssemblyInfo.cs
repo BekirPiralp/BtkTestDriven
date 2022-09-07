52a91ab514d5ef4b68bb042807f5b3d867b1e5b3
Bekir PİRALP <bpir33@gmail.com>
2022-09-07 18:17:14 +0300

    Assert methodları tanıtım

---
+++ Asserts.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Asserts.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Asserts.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb077e8d-b923-4fcb-8014-419a8ef067e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Asserts.Tests/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Asserts.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodMesaj() //test resultta hata mesajı verme
+        {
+            //Arrange
+            const double girilendeger = 16;
+            const double beklenen = 4;
+            //Act
+            double gerceklesen = Math.Sqrt(girilendeger);
+            //Assert
+            Assert.AreEqual(beklenen, gerceklesen, $"{girilendeger} syısının karekökü {beklenen} olmalıdır.");
+        }
+
+        [TestMethod]
+        public void TestMethodDelta()
+        {
+            //Hazırlık
+            const double beklenen = 3.1622;
+            const double delta = 0.0001;//Formül: |beklenen-gerçekleşen|<=delta
+            //Eylem
+            double gerceklesen = Math.Sqrt(10);
+            //Tez
+            Assert.AreEqual(beklenen, gerceklesen, delta);
+        }
+
+        [TestMethod]
+        public void TestMethodBüyükKüçükHarfeDuyarsız()
+        {
+            //Ayar
+            string belenen = "MERHABA", gerçekleşen = "merhaba";
+
+            //Tez
+            Assert.AreEqual(belenen, gerçekleşen, true);
+        }
+
+        [TestMethod]
+        public void TestMethodEsitDegilse()
+        {
+            //Hazırlık
+            const double beklenmeyen = 0;
+            double gerçekleşen = Math.Pow(5, 0);
+
+            //Tez
+            Assert.AreNotEqual(beklenmeyen, gerçekleşen);
+
+        }
+
+    }
+}
