7f3776a740188dcb393320134e0698868ef19c73
Bekir PİRALP <bpir33@gmail.com>
2022-09-07 19:31:41 +0300

    devam...

--- Asserts.Tests/UnitTest1.cs
+++ Asserts.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Asserts.Tests
@@ -57,7 +58,7 @@ namespace Asserts.Tests
         public void TestMethodReferanslarEşitmi()
         {
             //Hazırlık
-            Byte[] sayilar = new byte[] {1,2,3};
+            Byte[] sayilar = new byte[] { 1, 2, 3 };
 
             //Eylem
             var diğerSayılar = sayilar;
@@ -72,29 +73,99 @@ namespace Asserts.Tests
         }
 
         [TestMethod]
-        public void TestMEthodBaşarılıLakinYeterliDeğil()
+        public void TestMethodBaşarılıLakinYeterliDeğil()
         {
             Assert.AreEqual(1, 1); //Eşit olduğu için test başarılı bunun yerine kendi tezleriniz.
             Assert.Inconclusive("Bu test artık yerli değildir.");
         }
-        
+
+        [TestMethod]
+        public void VeriBeliritlenTipteMi()
+        {
+            //Hazırlık
+            var sayı = 5m;
+
+            //Bilgilendirme
+            Console.WriteLine("Sayı: " + sayı);
+
+            //Tez
+            Assert.IsInstanceOfType(sayı, typeof(decimal));
+            Assert.IsNotInstanceOfType(sayı, typeof(int));
+        }
+
+        [TestMethod]
+        public void DoğruMuVeyaYanlışMı()
+        {
+            Assert.IsTrue(10 % 2 == 0);
+            Assert.IsFalse(10 % 2 == 1);
+        }
+
+
+
+        List<String> _kulanıcılar = new List<string>(new string[] { "Engin",
+            "Salih",
+            "Ahmet" });
+
+        [TestMethod]
+        public void SırasıAynıVeDeğerleriEşitMi()
+        {
+            //Hazırlık
+            List<string> yeniKullanıcılar = new List<string>();
+
+            //Eylem
+
+            yeniKullanıcılar.Add("Engin");
+            yeniKullanıcılar.Add("Salih");
+            yeniKullanıcılar.Add("Ahmet");
+
+            /*
+            yeniKullanıcılar.Add("Engin");
+            yeniKullanıcılar.Add("Ahmet");
+            yeniKullanıcılar.Add("Salih");
+            */
+
+            //Tez
+            //CollectionAssert.AreNotEqual(_kulanıcılar, yeniKullanıcılar,"Sıraları veya değerleri eşit");
+            CollectionAssert.AreEqual(_kulanıcılar, yeniKullanıcılar, "Sıraları veya değerleri eşit değil");
+
+        }
+
+        [TestMethod]
+        public void DeğerleriEşitMi()
+        {
+            //Hazırlık
+            List<string> yeniKullanıcılar = new List<string>();
+
+            //Eylem
+
+            yeniKullanıcılar.Add("Engin");
+            yeniKullanıcılar.Add("Ahmet");
+            yeniKullanıcılar.Add("Salih");
+
+
+            //Tez
+            CollectionAssert.AreEquivalent(_kulanıcılar, yeniKullanıcılar, "Değerleri eşit değil");
+
+        }
+
     }
 
-    internal static class ExtensionMy {
+    internal static class ExtensionMy
+    {
         public static string convertString(this Byte[] bytes)
         {
             string result = "{ ";
-            foreach(Byte b in bytes)
+            foreach (Byte b in bytes)
             {
                 result += b.ToString() + " , ";
             }
 
-            result=result.Substring(0, result.Length - 2);
+            result = result.Substring(0, result.Length - 2);
 
             result += "}";
 
             return result;
         }
     }
-        
+
 }
