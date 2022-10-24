6d9cfa1a3dd7bd12e756379f8028fdd6e6ceedab
Bekir PİRALP <bpir33@gmail.com>
2022-10-24 10:30:03 +0300

    Test bağlam yapısına giriş yaptık

---
+++ TextContextTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TextContextTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TextContextTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("565c5617-77bf-4f1b-9419-c138d42fadf4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TextContextTests/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TextContextTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        private String Anahtar = "bilgi";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestContext.WriteLine("-- Test Initialize --\n");
+            TestContext.WriteLine("Test Adı:    \t{0}", TestContext.TestName);
+            TestContext.WriteLine("Test Durumu: \t{0}", TestContext.CurrentTestOutcome);
+            TestContext.Properties[Anahtar] = "Bu bir ekstra bilgidir.";
+            TestContext.WriteLine("");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestContext.WriteLine("\t-->> Test\n");
+            TestContext.WriteLine("\tTest Adı:    \t{0}", TestContext.TestName);
+            TestContext.WriteLine("\tTest Durumu: \t{0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("\tTest Sınıfı: \t{0}", TestContext.FullyQualifiedTestClassName);
+            TestContext.WriteLine("\tTest Dir:    \t{0}", TestContext.TestDir);
+            TestContext.WriteLine("\tTest LogDir: \t{0}", TestContext.TestLogsDir);
+            TestContext.WriteLine("\tTest RunDir: \t{0}", TestContext.TestRunDirectory);
+            TestContext.WriteLine("\tTest Bilgi:  \t{0}", TestContext.Properties[Anahtar]);
+            TestContext.WriteLine("");
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestContext.WriteLine("-- Test Cleanup --\n");
+            TestContext.WriteLine("Test Adı:    \t{0}", TestContext.TestName);
+            TestContext.WriteLine("Test Durumu: \t{0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Test Bilgi:  \t{0}", TestContext.Properties[Anahtar]);
+            TestContext.WriteLine("");
+        }
+    }
+}
